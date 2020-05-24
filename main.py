7bf3ae560d5a14edc093f5a1bc51dd2afec66d22
Sang min Lee <sang.m.lee@mail.mcgill.ca>
2020-05-24 01:23:42 -0400

    all requirements set

--- main.py
+++ main.py
@@ -55,47 +55,7 @@ def upload_process():
         )
         return response
 
-# @app.route("/api/food")
-# def show_foods():
-#     res = find_recipes()
-#     json_data = json.loads(res.text)
-#     print(json_data)
-#     return jsonify(json_data)
-#
-# @app.route("/webhook", methods=["POST"])
-# def webhook():
-#     req = request.get_json(silent=true, force=True)
-#     print("Request: ")
-#     print(json.dumps(req, indent=4))
-#     res = makeWebhookResult(req)
-#     res = json.dumps(res, indent=4)
-#     print(res)
-#     r = make_response(res)
-#     r.headers["Content-Type"] = 'application/json'
-#     return r
-#
-# def makeWebhookResult(req):
-#     if req.get("result").get("action") != "recipes":
-#         return {}
-#     result = req.get("result")
-#     parameters = result.get("parameters")
-#     zone = parameters.get("ingredients")
-#     speech = "The food I can recommend you is" + zone + ", ok?"
-#     print("response: ")
-#     print(speech)
-#     return {
-#         "speech": speech,
-#         "displayText": speech,
-#         "source": "ingredients"
-#     }
-
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    # app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
